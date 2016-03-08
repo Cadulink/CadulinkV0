3fb5a84cbae4300a90432ca771113823b376f254
DELPIERRE Michel <delpierre.m@pop.eu.com>
2016-03-08 10:45:09 +0100

    connection page & forgot password +lib

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,12 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
+
+
+
+
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +27,19 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+// connection
+  $stateProvider.state('connect', {
+    url: '/connect',
+    templateUrl: 'templates/connect.html'
+  })
+// mots de passe oublié
+  $stateProvider.state('forgotPassword', {
+    url: '/forgotPassword',
+      templateUrl: 'templates/forgotPassword.html'
+  })
+
+  $urlRouterProvider.otherwise('/connect')
+
+});
