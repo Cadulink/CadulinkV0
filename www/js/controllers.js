c3230810eb08b69ded00c055c13eeb0a6696cba7
Klorel <klorel@Infinity.fr>
2016-03-09 12:06:28 +0100

    localStorage opérationnel

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,6 +15,22 @@ angular.module('starter.controllers', [])
   $scope.CloseModal = function(){
     $scope.modal.hide();
   };
+
+  $scope.submit = function(email, password, nom, prenom, profession, lieuExercice){
+
+    var perso = {
+      'email': email,
+      'password': password,
+      'nom': nom,
+      'prenom': prenom,
+      'profession': profession,
+      'lieuExercice': lieuExercice
+    };
+
+    window.localStorage.setItem('perso', JSON.stringify(perso));
+
+  };
+
 })
 
 /*.controller('RegisterCtrl', function($scope, $ionicLoading, $state, $location){
